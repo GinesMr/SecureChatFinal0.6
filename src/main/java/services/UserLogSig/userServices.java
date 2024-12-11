077caa969cd844e1b2e00f814369f7af687549e1
Ginés Martínez Ruiz <logxoli2004@gmail.com>
2024-12-11 04:33:31 +0100

    Update userServices.java

--- src/main/java/services/UserLogSig/userServices.java
+++ src/main/java/services/UserLogSig/userServices.java
@@ -4,10 +4,56 @@
  */
 package services.UserLogSig;
 
+import model.user;
+import repository.UserController.ControladorPersistenciaController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author gines
  */
 public class userServices {
-    
+
+        ControladorPersistenciaController controladorPersistenciaController = new ControladorPersistenciaController();
+
+        public void crearusu(String Username ,String password){
+
+            if(existe(Username,password)== false){
+                user user = new user(Username,password);
+                    controladorPersistenciaController.crear(user);
+            }
+
+        }
+
+
+    public boolean existe(String username, String password) {
+        List<user> usuarios = controladorPersistenciaController.buscarTodos();
+
+        for (user usuario : usuarios) {
+            if (usuario.getUsername().equals(username) && usuario.getPublicKey().equals(password)) {
+                System.out.println("Usuario encontrado: " + usuario.getUsername());
+                return true;
+            }
+        }
+
+        System.out.println("Usuario no encontrado.");
+        return false;
+    }
+
+    public boolean existeusername(String username) {
+        List<user> usuarios = controladorPersistenciaController.buscarTodos();
+
+        for (user usuario : usuarios) {
+            if (usuario.getUsername().equals(username)) {
+                System.out.println("Usuario con cuenta: " + usuario.getUsername());
+                return true;
+            }
+        }
+
+        System.out.println("Usuario sin cuenta.");
+        return false;
+    }
+
 }
